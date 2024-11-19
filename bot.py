8dfbd28793a53cbebb1c239db579a4217845e8cc
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-11-19 22:30:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -124,7 +124,7 @@ def plan_details(client, callback_query):
     f"\n✶ Daily Upload: {details['daily_upload_limit'] // (1024 ** 3)} GB\n"
     f"✶ File Upload Size: {details['file_size_limit'] // (1024 ** 3)} GB\n"
     f"✶ Parallel Processes: {details['parallel_process']}\n"
-    f"✶ Time Gap: {'No' if details['time_gap'] == 0 else f'{details[\'time_gap\'] // 60} Minutes'}\n"
+    f"✶ Time Gap: {'No' if details['time_gap'] == 0 else f'yes'}\n"
     f"✶ Auto Watermark: {'Yes' if details['auto_watermark'] else 'No'}"
 )
     keyboard = InlineKeyboardMarkup([
