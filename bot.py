7ba927e895703bf10f7d41a9e7a9dced9eaae93b
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-02-13 21:28:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ api_id = 15191874  # Replace with your API ID
 api_hash = "3037d39233c6fad9b80d83bb8a339a07"  # Replace with your API Hash
 session_string = "BQDnz0IAbrET1b89Vlg4OYD7UScdllLaVMO64LMokSL8ux2FRwYdOhdTvw-LxHoZ1XNZ3s5T8sX-IxZcvxSxrsvLYHjPx1imajxvKVcixAd4_OXsOExJ9vgfh3T3-y6MMbrq6jnhNEbqfJxNIZxj5N5hFQbtGrwNnHElr7kcOM-yJfm_90xrLhVrW9ILvCBs9MA5taV5EJ9uFX4usuEfyalQ4FMGnnb-IGUiXlHeY3xjw3tVww690U5mOPTHCnKxrjMTSic3QkthgJMeFS_VzwlON9Xts4wvxCni78QElDsCeB0Va0FVoo1gJawGcBs8ng1cPWIYEQ_akySd6tz-TVJlGu-DcgAAAAFNo2ggAA"  # Replace with your actual session string
 
-group_id = -1001234567890  # Replace with your target group ID
+group_id = -1002315666631  # Replace with your target group ID
 message_text = "This is an automated message!"  # Customize your message
 
 app = Client("my_userbot", api_id, api_hash, session_string=session_string)
