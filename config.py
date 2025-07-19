e6a909fbc1ab2cbbd46a31b9141e7b750c64c20a
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-07-19 15:57:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-API_ID = 123456
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
-ADMINS = [123456789, 987654321]  # List of Telegram user IDs allowed to control the bot
+API_ID = 15191874
+API_HASH = "3037d39233c6fad9b80d83bb8a339a07"
+BOT_TOKEN = "6677023637:AAHKWIl3PxwiF9HhVfhiC4drnTZyZCYPtGE"
+ADMINS = [5597521952]  # List of Telegram user IDs allowed to control the bot
